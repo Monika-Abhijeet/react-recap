aad9a1dec781491eb1d162c288c35b6fd9859539
Monika Abhijeet <monikaabhijeet@Monikas-MacBook-Pro.local>
2022-09-30 17:14:12 +0530

    axios

--- src/components/employeeDetails/employeeDetails.js
+++ src/components/employeeDetails/employeeDetails.js
@@ -1,4 +1,39 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 function EmployeeDetails() {
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      console.log(response.data);
+    });
+
+    // let request = {
+    //   id: 1,
+    //   userId: 2,
+    //   title: "Monika",
+    //   body: "Hello Monika",
+    // };
+    // axios
+    //   .post("https://jsonplaceholder.typicode.com/posts", request)
+    //   .then((response) => console.log(response));
+
+    // let request = {
+    //   id: 1,
+    //   userId: 2,
+    //   title: "Monika Abhijeet",
+    //   body: " welcome to react session",
+    // };
+
+    // axios
+    //   .put("https://jsonplaceholder.typicode.com/posts/1", request)
+    //   .then((response) => {
+    //     console.log(response);
+    //   });
+    axios.delete("https://jsonplaceholder.typicode.com/posts/1").then((res) => {
+      console.log(res);
+    });
+  });
+
   return <h1>Employee Details</h1>;
 }
 
