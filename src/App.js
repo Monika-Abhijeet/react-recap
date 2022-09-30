bb6db8940593784a1b8a5bc2759acbec5466b3a8
Monika Abhijeet <monikaabhijeet@Monikas-MacBook-Pro.local>
2022-09-30 10:27:02 +0530

    added footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from "./components/header/header";
-
+import Footer from "./components/footer/footer";
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <h1>Hello Monika</h1>
+      <Footer></Footer>
     </div>
   );
 }
