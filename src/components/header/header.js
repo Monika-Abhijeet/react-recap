8f724cd082b1a5990fe1e4ff454eb49bf9f8e52e
Monika Abhijeet <monikaabhijeet@Monikas-MacBook-Pro.local>
2022-09-30 10:14:55 +0530

    initial commit

---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import "./header.css";
+
+function Header() {
+  return (
+    <div className="header-wrapper">
+      <h1>This is a header component</h1>
+    </div>
+  );
+}
+
+export default Header;
